148ad163956b2c5ce90e6f32a6bf427081c104cc
Abdalla Alhag <alhagabdalla@gmail.com>
2023-07-03 11:52:31 -0700

    clone complete

--- script.js
+++ script.js
@@ -1,11 +1,144 @@
 const CANVAS_BORDER_COLOR = 'black';
 const CANVAS_BACKGROUND_COLOR = 'white';
+const SNAKE_COLOR = 'lightgreen';
+const SNAKE_BORDER_COLOR = 'darkgreen';
 
-let gameCanvas = document.getElementById('gameCanvas');
-let ctx = gameCanvas.getContext('2d');
+var gameCanvas = document.getElementById('gameCanvas');
+var ctx = gameCanvas.getContext('2d');
 
-ctx.fillStyle = CANVAS_BACKGROUND_COLOR;
-ctx.strokestyle = CANVAS_BORDER_COLOR;
 
-ctx.fillRect(0,0,gameCanvas.width, gameCanvas.height);
-ctx.strokeRect(0,0, gameCanvas.width, gameCanvas.height);
+function clearCanvas() {
+    ctx.fillStyle = CANVAS_BACKGROUND_COLOR;
+    ctx.strokestyle = CANVAS_BORDER_COLOR;
+
+    ctx.fillRect(0,0,gameCanvas.width, gameCanvas.height);
+    ctx.strokeRect(0,0, gameCanvas.width, gameCanvas.height);
+}
+
+
+let snake = [
+    {x:150, y:150},
+    {x:140, y:150},
+    {x:130, y:150},
+    {x:120, y:150},
+    {x:110, y:150},
+];
+
+let dx = 10;
+let dy = 0;
+let score = 0;
+
+
+
+main();
+createFood();
+document.addEventListener("keydown", changeDirection);
+drawSnake();
+
+function main() {
+    if(didGameEnd()) return;
+    setTimeout(function onTick() {
+        clearCanvas();
+        drawFood();
+        advanceSnake();
+        drawSnake();
+
+        main();
+    },100)
+}
+
+function drawSnakePart(snakePart) {
+    ctx.fillStyle = 'lightgreen';
+    ctx.strokestyle = 'darkgreen';
+
+    ctx.fillRect(snakePart.x, snakePart.y, 10, 10);
+    ctx.strokeRect(snakePart.x, snakePart.y, 10, 10);
+}
+
+function drawSnake() {
+    snake.forEach(drawSnakePart);
+}
+
+function advanceSnake() {
+    const head = {x: snake[0].x + dx, y: snake[0].y + dy};
+    
+    snake.unshift(head);
+
+    const didEatFood = snake[0].x === foodX && snake[0].y === foodY;
+    if (didEatFood) {
+        score += 10;
+        document.getElementById('score').innerHTML = score;
+
+        createFood();
+    } else {
+        snake.pop();
+    }
+}
+
+function changeDirection(e) {
+    const LEFT_KEY = 37;
+    const RIGHT_KEY = 39;
+    const UP_KEY = 38;
+    const DOWN_KEY = 40;
+
+    const keyPressed = e.keyCode;
+    const goingUp = (dy === -10);
+    const goingDown = (dy === 10);
+    const goingRight = (dx === 10);
+    const goingLeft = (dx === -10);
+
+    if (keyPressed === LEFT_KEY && !goingRight) {
+        dx = -10;
+        dy = 0;
+      }
+      if (keyPressed === UP_KEY && !goingDown) {
+        dx = 0;
+        dy = -10;
+      }
+      if (keyPressed === RIGHT_KEY && !goingLeft) {
+        dx = 10;
+        dy = 0;
+      }
+      if (keyPressed === DOWN_KEY && !goingUp) {
+        dx = 0;
+        dy = 10;
+      }
+}
+
+function randomTen(min, max) {
+    return Math.round((Math.random() * (max-min) + min) / 10) * 10;
+}
+
+function createFood() {
+    foodX = randomTen(0, gameCanvas.width - 10);
+    foodY = randomTen(0, gameCanvas.height - 10);
+
+    snake.forEach(function isFoodOnSnake(part) {
+        const foodIsOnSnake = part.x == foodX && part.y == foodY;
+        if (foodIsOnSnake)
+            createFood();
+    });
+}
+
+function drawFood() {
+    ctx.fillStyle = 'red';
+    ctx.strokestyle = 'darkred';
+    ctx.fillRect(foodX, foodY, 10, 10);
+    ctx.strokeRect(foodX, foodY, 10, 10);
+}
+
+function didGameEnd() {
+    for (let i = 4; i < snake.length; i++) {
+        const didCollide = ( (snake[i].x === snake[0].x) &&
+        (snake[i].y === snake[0].y) );
+
+        if (didCollide) return true;
+    }
+
+    const hitLeftWall = snake[0].x < 0;
+    const hitRightWall = snake[0].x > gameCanvas.width - 10;
+    const hitTopWall = snake[0].y < 0;
+    const hitBottomWall = snake[0].y > gameCanvas.height - 10;
+
+    return hitLeftWall || hitRightWall || hitTopWall || hitBottomWall;
+}
